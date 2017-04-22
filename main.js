bbe618fdb5e3b1440d4fb77b2d9c9381f41fa179
Luke Henwood <luke8h@gmail.com>
2017-04-22 14:07:31 -0500

    made parameters for the constructor

---
+++ main.js
@@ -0,0 +1,10 @@
+var Car = function(wheels, seats, engines) {
+  //Change this constructor
+  this.wheels = wheels;
+   this.seats = seats;
+   this.engines = engines;
+};
+console.log(Car);
+//Try it out here
+var myCar = new Car(6, 3, 1);
+console.log(myCar);
